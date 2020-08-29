5b518dee9094d1db55a6337bd8a3464eca78f804
kspringh <kz1000@hotmail.com>
2020-08-29 15:10:29 -0400

    ant build works

---
+++ src/main/java/com/kenspring/foo/Driver.java
@@ -0,0 +1,13 @@
+/*
+ * Driver class for build test developement
+ */ 
+
+package com.kenspring.foo;
+
+public class Driver{
+  public static void main(String arg[]){
+
+    System.out.println("com.kenspring.foo.Driver running");
+
+  }
+}
